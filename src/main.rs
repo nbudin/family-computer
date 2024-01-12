137a418ac30bc92e8a9c5bee1ab67040f996dabd
Nat Budin <nbudin@actbluetech.com>
2024-01-12 10:18:40 -0800

    Open a native file picker if no rom specified on cli

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod gui;
 mod nes;
 mod ppu;
 
-use std::{env, path::Path};
+use std::{env, path::PathBuf, str::FromStr};
 
 use iced::{Application, Settings};
 
@@ -19,16 +19,27 @@ use crate::{
 };
 
 pub fn main() -> Result<(), iced::Error> {
+  if env::var("SMOL_THREADS").is_err() {
+    env::set_var("SMOL_THREADS", "4");
+  }
+
   let args = env::args().into_iter().collect::<Vec<_>>();
-  let Some(rom_path) = args.get(1).map(Path::new) else {
-    println!("Please specify a ROM path");
-    return Ok(());
+  let rom_path = match args
+    .get(1)
+    .map(|arg| PathBuf::from_str(arg.as_str()).unwrap())
+  {
+    Some(rom_path) => rom_path,
+    None => native_dialog::FileDialog::new()
+      .add_filter("iNES ROM", &["nes"])
+      .set_title("Choose a NES ROM file to run")
+      .show_open_single_file()
+      .unwrap()
+      .unwrap(),
   };
 
   println!("Loading {}", rom_path.display());
 
   let rom = INESRom::from_file(&rom_path).unwrap();
-  // let rom = INESRom::from_reader(&mut include_bytes!("../dk.nes").as_slice()).unwrap();
   println!("Using mapper ID {}", rom.mapper_id);
 
   EmulatorUI::run(Settings::with_flags(EmulatorUIFlags::new(Box::new(
